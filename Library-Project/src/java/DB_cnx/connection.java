093b5814ea68cd496a3044325adbd63e201d16c3
simoxxhunter <94250469+simoxxhunter@users.noreply.github.com>
2024-04-17 16:03:32 +0000

    Creation de la fichier cnx et insertion

---
+++ Library-Project/src/java/DB_cnx/connection.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DB_cnx;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author user
+ */
+public class connection {
+      private static final String URL = "jdbc:postgresql://localhost:5432/DB_pharmacy";
+        private static final String USER = "admin";
+        private static final String PASS = "admin";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+
+            Class.forName("org.postgresql.Driver");
+
+            connection = DriverManager.getConnection(URL, USER, PASS);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println("Error cant connet : " + e.getMessage());
+        }
+        return connection;
+    }
+
+}
